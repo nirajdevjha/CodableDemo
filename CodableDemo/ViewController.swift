52ef4c04522df7af898c800859590d81100075b4
nirajdevjha <nirajdev.jha@gmail.com>
2019-04-03 11:53:43 +0530

    Added a tab for topRated and handled error cases.

--- CodableDemo/ViewController.swift
+++ CodableDemo/ViewController.swift
@@ -18,18 +18,41 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(didTappedCredits))
         // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 //We're ok to parse
                 parse(json: data)
+                return
             }
         }
+        showError()
     }
     
     // MARK: - Private
+    
+    
+    @objc func didTappedCredits() {
+        let alertController = UIAlertController(title: nil, message: "the data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func showError() {
+        let alertController = UIAlertController(title: "Loading error", message: "There was a problem loading the feed, please check your connection and try again", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true, completion: nil)
+    }
+    
     
     func parse(json: Data) {
         let decoder = JSONDecoder()
